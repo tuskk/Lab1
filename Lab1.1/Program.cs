cfc151bf6487e36811ad6c8e6d285cf0a9048046
Mega <Mega@TUF15>
2023-02-23 08:17:48 +0200

    Добавьте файлы проекта.

---
+++ Lab1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using lab;
+
+namespace Lab1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            task();
+
+        }
+        static void task()
+        {
+            SClass test = new SClass("SHdRVBAHskaBJSyHfD");
+            Console.WriteLine("String: " + test.ToString());
+            Console.WriteLine("Sorted: " + test.sort());
+            Console.WriteLine("Length: " + test.length());
+
+        }
+    }
+}
---
+++ Lab1.1/SClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    class SClass
+    {
+        public string StoredText;
+        public SClass(string NString)
+        {
+            Regex regex=new Regex(@"[\p{Ll}]+");
+            MatchCollection matchedAuthors = regex.Matches(NString);
+            StoredText = "";
+            foreach (Match match in matchedAuthors)
+            {
+                StoredText += match.Value;
+            }
+        }
+        public int length()
+        {
+            return StoredText.Length;
+        }
+        public string sort()
+        {
+            return StoredText = string.Concat(StoredText.OrderBy(ch => ch));
+        }
+        public void outputToConsole()
+        {
+            Console.WriteLine(StoredText);
+        }
+        public override string ToString()
+        {
+            return StoredText;
+        }
+    }
+}
